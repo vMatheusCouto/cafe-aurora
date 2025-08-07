a467061122397187f6faa66b10ade8ff34333e48
Matheus Couto <ctheusg@hotmail.com>
2025-08-07 10:17:01 -0300

    fix: fix configjs url

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const url = {
   home: "/cafe-aurora/",
-  about: "/cafe-aurorapages/about.html",
-  catalogue: "/cafe-aurorapages/catalogue.html",
-  tips: "/cafe-aurorapages/tips.html",
-  events: "/cafe-aurorapages/events.html",
-  contact: "/cafe-aurorapages/contacts.html"
+  about: "/cafe-aurora/pages/about.html",
+  catalogue: "/cafe-aurora/pages/catalogue.html",
+  tips: "/cafe-aurora/pages/tips.html",
+  events: "/cafe-aurora/pages/events.html",
+  contact: "/cafe-aurora/pages/contacts.html"
 }
